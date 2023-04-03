0e0a37fb1032f7f51718ddaa7b2a27670a794fbc
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-04 07:46:43 +0900

    14.#12_Repositoriesフォルダーの作成

---
+++ NDDD.Domain/Repositories/IMeasureRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Repositories
+{
+    public interface IMeasureRepository
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>Measureテーブルからとってきたデータを詰め込んで運ぶクラス</returns>
+        MeasureEntity GetLatest();
+    }
+}
