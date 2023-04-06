558bc71157d585b934a5d1f68ee249381930b23f
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-07 07:47:50 +0900

    31.#29_FakePathを設定ファイルとSharedに移す

--- NDDD.Domain/Shared.cs
+++ NDDD.Domain/Shared.cs
@@ -11,5 +11,8 @@ namespace NDDD.Domain
     {
         public static bool IsFake { get; } =
             ConfigurationManager.AppSettings["IsFake"] == "1";
+        public static string FakePath { get; } =
+            ConfigurationManager.AppSettings["FakePath"];
+
     }
 }
--- NDDD.Infrastructure/Fake/MeasureFake.cs
+++ NDDD.Infrastructure/Fake/MeasureFake.cs
@@ -1,4 +1,5 @@
-﻿using NDDD.Domain.Entities;
+﻿using NDDD.Domain;
+using NDDD.Domain.Entities;
 using NDDD.Domain.Repositories;
 using System;
 using System.Collections.Generic;
@@ -10,14 +11,13 @@ namespace NDDD.Infrastructure.Fake
 {
     internal class MeasureFake : IMeasureRepository
     {
-        internal const string FakePath = @"C:\Users\kyota\source\repos\NDDD\NDDD.Infrastructure\NDDDFake\";
         public MeasureEntity GetLatest()
         {
 
             try
             {
                 var lines = System.IO.File.ReadAllLines(
-                    FakePath + "MeasureFake.csv"); // ファイル名はクラス名.拡張子
+                    Shared.FakePath + @"\" + "MeasureFake.csv"); // ファイル名はクラス名.拡張子
                 var value = lines[0].Split(','/*値にカンマはない想定*/);    // 1件目だけとる
                 return new MeasureEntity(
                     Convert.ToInt32(value[0]),
