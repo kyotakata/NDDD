b43b75d8098c0c95be4815a468ccf4ce668203a3
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-07 07:29:55 +0900

    30.#28_Fakeデータを切り替える方法

--- NDDD.Infrastructure/Fake/MeasureFake.cs
+++ NDDD.Infrastructure/Fake/MeasureFake.cs
@@ -10,12 +10,28 @@ namespace NDDD.Infrastructure.Fake
 {
     internal class MeasureFake : IMeasureRepository
     {
+        internal const string FakePath = @"C:\Users\kyota\source\repos\NDDD\NDDD.Infrastructure\NDDDFake\";
         public MeasureEntity GetLatest()
         {
-            return new MeasureEntity(
-                10,
-                Convert.ToDateTime("2020/12/12 12:34:56"),
-                123.341f);
+
+            try
+            {
+                var lines = System.IO.File.ReadAllLines(
+                    FakePath + "MeasureFake.csv"); // ファイル名はクラス名.拡張子
+                var value = lines[0].Split(','/*値にカンマはない想定*/);    // 1件目だけとる
+                return new MeasureEntity(
+                    Convert.ToInt32(value[0]),
+                    Convert.ToDateTime(value[1]),
+                    Convert.ToSingle(value[2]));
+            }
+            catch
+            {
+                // ファイルがない場合はCatchした値が画面にデフォルトで出るようにする
+                return new MeasureEntity(
+                    10,
+                    Convert.ToDateTime("2020/12/12 12:34:56"),
+                    123.341f);
+            }
         }
     }
 }
