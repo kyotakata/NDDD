ae4e35c2bc655818fef6e558ec3c954741617744
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-10 07:53:36 +0900

    38.#36_ValueObjectを作成する

---
+++ NDDD.Domain/ValueObjects/AreaId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.ValueObjects
+{
+    public sealed class AreaId
+    {
+        /// <summary>
+        /// 完全コンストラクタパターン。コンストラクタで値設定後は、値を変更できないクラスのこと
+        /// </summary>
+        /// <param name="value"></param>
+        public AreaId(int value)
+        {
+            Value = value;
+        }
+
+        /// <summary>
+        /// 型はデータベースに合わせる。
+        /// </summary>
+        public int Value { get; }
+    }
+}
