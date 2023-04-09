6c2b7a16f6149b3b700c163b5e069a8c15a720fc
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-10 07:27:35 +0900

    33.#31_Sharedクラスの活用方法

---
+++ NDDD.WinForm/Views/LoginView.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace NDDD.WinForm.Views
+{
+    partial class LoginView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(22, 115);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(245, 23);
+            this.LoginButton.TabIndex = 14;
+            this.LoginButton.Text = "ログイン";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(94, 58);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.ReadOnly = true;
+            this.PasswordTextBox.Size = new System.Drawing.Size(155, 22);
+            this.PasswordTextBox.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 15);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "パスワード";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Location = new System.Drawing.Point(94, 30);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.ReadOnly = true;
+            this.LoginTextBox.Size = new System.Drawing.Size(155, 22);
+            this.LoginTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "ログインID";
+            // 
+            // LoginView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(316, 184);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "LoginView";
+            this.Text = "LoginView";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
