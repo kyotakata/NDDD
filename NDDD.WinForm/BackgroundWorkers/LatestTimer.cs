8b85fb04e97bda63ec153156761cebaafd7c1c38
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-12 20:01:33 +0900

    56.#62_タイマークラスの作成

--- NDDD.WinForm/BackgroundWorkers/LatestTimer.cs
+++ NDDD.WinForm/BackgroundWorkers/LatestTimer.cs
@@ -2,11 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NDDD.WinForm.BackgroundWorkers
 {
-    internal class LatestTimer
+    internal static class LatestTimer
     {
+        private static System.Threading.Timer _timer;
+        private static bool _isWork = false;
+
+        static LatestTimer()
+        {
+            _timer = new Timer(Callback);
+        }
+
+        internal static void Start()
+        {
+            _timer.Change(10000, 10000);
+        }
+
+        internal static void Stop()
+        {
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
+        private static void Callback(object o)
+        {
+            if (_isWork)
+            {
+                return;
+            }
+
+
+            try
+            {
+                _isWork = true;
+                //何か処理する
+            }
+            finally
+            {
+                // 最後に必ずfalse
+                _isWork = false;
+            }
+        }
     }
 }
