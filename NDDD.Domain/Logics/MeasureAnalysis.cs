815a5b1e328db842f0f56db207384417ffb1ce3b
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-04-12 21:45:35 +0900

    58.#54_Logics

---
+++ NDDD.Domain/Logics/MeasureAnalysis.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NDDD.Domain.Logics
+{
+    public static class MeasureAnalysis
+    {
+        /// <summary>
+        /// 例えば、データ5年分分析するメソッド
+        /// </summary>
+        /// <param name="value1"></param>
+        /// <param name="value2"></param>
+        /// <param name="value3"></param>
+        /// <param name="value4"></param>
+        /// <param name="value5"></param>
+        /// <returns></returns>
+        public static float Execute(
+            float value1,
+            float value2,
+            float value3,
+            float value4,
+            float value5
+            )
+        {
+            return 999f;
+        }
+    }
+}
